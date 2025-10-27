22eae4091f38dab18463b755e21aaa391abcbe08
TanyaZekter <tetiana.zekter.ki.2022@lpnu.ua>
2025-10-27 16:40:16 +0200

    Rewrote tests using GoogleTest
    Refactored unit tests to use Google Test framework and added multiple test cases for addition.

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
-#include <string>
+#include <gtest/gtest.h>
 #include "math_operations.h"
 
-static bool test_add(const std::string& name, int a, int b, int expected) {
-    int r = add(a, b);
-    if (r != expected) {
-        std::cout << "[FAIL] " << name << ": add(" << a << ", " << b << ") == " << r << " (expected " << expected << ")\n";
-        return false;
-    }
-    std::cout << "[PASS] " << name << "\n";
-    return true;
+TEST(Addition, PositiveNumbers) {
+    EXPECT_EQ(add(2, 3), 5);
 }
 
-int main() {
-    bool ok = true;
-    ok &= test_add("positive numbers", 2, 3, 5);
-    ok &= test_add("negative numbers", -2, -3, -5);
-    ok &= test_add("mixed", -5, 10, 5);
-    ok &= test_add("zero", 0, 0, 0);
-    ok &= test_add("large", 1000000, 2000000, 3000000);
+TEST(Addition, NegativeNumbers) {
+    EXPECT_EQ(add(-2, -3), -5);
+}
 
-    if (!ok) {
-        std::cout << "Some tests failed\n";
-        return 1;
-    }
+TEST(Addition, MixedNumbers) {
+    EXPECT_EQ(add(-5, 10), 5);
+}
 
-    std::cout << "All tests passed\n";
-    return 0;
+TEST(Addition, Zero) {
+    EXPECT_EQ(add(0, 0), 0);
+}
+
+TEST(Addition, LargeNumbers) {
+    EXPECT_EQ(add(1000000, 2000000), 3000000);
 }
